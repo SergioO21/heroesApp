72befebf33cd6e8e65b583e61de6cdaf1d0338ec
Sergio Orejarena <sergioorueda@gmail.com>
2024-08-15 08:37:54 -0500

    Set up Material and PrimeFlex packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, SharedModule],
-	providers: [],
+	imports: [
+    AppRoutingModule,
+    BrowserModule,
+    SharedModule,
+  ],
+	providers: [
+    provideAnimationsAsync()
+  ],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}
